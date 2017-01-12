2ed35f72d8a483a9a237468ef0747c7fe69047ce
c00z <zcusimano@gmail.com>
2017-01-11 16:25:21 -0800

    Finished hangular

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -2,4 +2,14 @@ console.log('app.js loaded!');
 
 // initialize the application
 angular
-  .module("hangmanApp", []);
+  .module("hangmanApp", [])
+  .controller("HangmanController", HangmanController);
+
+  function HangmanController() {
+    var vm = this;
+    // initialize a new game
+    vm.game = new HangmanGame("elephant");
+    // console.log(vm.game.guess("e"));
+    // console.log(vm.game.guess("j"));
+
+  }
